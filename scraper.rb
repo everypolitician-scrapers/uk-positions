073da00dd845a428616074b00b134a17e02167fc
Tony Bowden <tony@tmtm.com>
2017-02-07 14:14:59 +0000

    Clean out database on each run

--- scraper.rb
+++ scraper.rb
@@ -8,6 +8,7 @@ require 'scraperwiki'
 
 require_relative 'lib/politician'
 
+ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 house = EveryPolitician::Index.new.country('UK').lower_house
 house.popolo.persons.map(&:wikidata).compact.each_slice(100) do |wanted|
   data = Wikisnakker::Politician.find(wanted).flat_map(&:positions).compact

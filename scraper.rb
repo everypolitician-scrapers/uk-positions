b80e58f605219e36d03bf84677bdba1a4159b5cc
Tony Bowden <tony@tmtm.com>
2017-02-07 14:12:58 +0000

    Harmonise with other P39 scrapers

--- scraper.rb
+++ scraper.rb
@@ -3,70 +3,13 @@
 # frozen_string_literal: true
 
 require 'everypolitician'
+require 'pry'
 require 'scraperwiki'
 
-require 'pry'
-
-module Wikisnakker
-  class Item
-    QUALIFIERS = {
-      P102:  :party,
-      P155:  :follows,
-      P156:  :followed_by,
-      P194:  :body,
-      P580:  :start_date,
-      P582:  :end_date,
-      P642:  :of,
-      P768:  :constituency,
-      P1365: :replaces,
-      P1366: :replaced_by,
-      P2715: :election,
-      P2937: :term,
-    }.freeze
-
-    def positions
-      return [] if self.P39s.empty?
-      self.P39s.map do |posn|
-        quals = posn.qualifiers
-        qdata = quals.properties.partition { |p| QUALIFIERS[p] }
-        qgood = qdata.first.map { |p| [QUALIFIERS[p], quals[p].value.to_s] }.to_h
-        warn "#{id}: unknown #{qdata.last.join(', ')} as #{posn.value}" unless qdata.last.empty?
-
-        {
-          id:          id,
-          position:    posn.value.id,
-          label:       posn.value.to_s,
-          description: posn.value.description(:en).to_s,
-          start_date:  '' # need _something_ here so we can key on it
-        }.merge(qgood) rescue {}
-      end
-    end
-  end
-end
-
-module Wikidata
-  require 'wikisnakker'
-
-  class Positions
-    def initialize(ids:)
-      @ids = ids
-    end
-
-    def positions
-      wikisnakker_items.flat_map(&:positions).compact
-    end
-
-    private
-
-    attr_reader :ids
-
-    def wikisnakker_items
-      @wsitems ||= Wikisnakker::Item.find(ids)
-    end
-  end
-end
+require_relative 'lib/politician'
 
 house = EveryPolitician::Index.new.country('UK').lower_house
-wanted = house.popolo.persons.map(&:wikidata).compact
-data = Wikidata::Positions.new(ids: wanted).positions
-ScraperWiki.save_sqlite(%i(id position start_date), data)
+house.popolo.persons.map(&:wikidata).compact.each_slice(100) do |wanted|
+  data = Wikisnakker::Politician.find(wanted).flat_map(&:positions).compact
+  ScraperWiki.save_sqlite(%i(id position start_date), data)
+end
